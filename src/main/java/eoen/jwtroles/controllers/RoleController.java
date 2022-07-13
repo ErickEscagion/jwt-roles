fbe67da0f31e2349587ec0944919e75733fa513f
eoen <eoen@gft.com>
2022-07-13 16:02:25 -0300

    role documentation to swagger

--- src/main/java/eoen/jwtroles/controllers/RoleController.java
+++ src/main/java/eoen/jwtroles/controllers/RoleController.java
@@ -2,6 +2,7 @@ package eoen.jwtroles.controllers;
 
 import javax.validation.Valid;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
@@ -17,12 +18,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import eoen.jwtroles.dtos.ErrorsDTO;
 import eoen.jwtroles.dtos.RoleRequestDTO;
 import eoen.jwtroles.dtos.RoleResponseDTO;
 import eoen.jwtroles.entities.Role;
 import eoen.jwtroles.exception.EntityNotFoundException;
 import eoen.jwtroles.mappers.RoleMapper;
 import eoen.jwtroles.services.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Content;
 
 @RestController
 @RequestMapping("v1/role")
@@ -32,29 +39,61 @@ public class RoleController {
 	@Autowired
 	private RoleService roleService;
 
+	@Operation(summary = "Post Role", description = "Route to post a new Role", tags = "Role")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Role created!"),
+			@ApiResponse(responseCode = "400", description = "Argument not valid!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true)))
+	})
 	@PostMapping
 	public ResponseEntity<RoleResponseDTO> postNewRole(@Valid @RequestBody RoleRequestDTO dto) {
 		Role roleSaved = roleService.postNewRole(RoleMapper.fromDtoToRole(dto));
 		return ResponseEntity.ok(RoleMapper.fromRoleToResponse(roleSaved));
 	}
 
+	@Operation(summary = "Get Role", description = "Route to get all Roles", tags = "Role")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Get roles success!", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "500", description = "Parameter from pageable invalid!", content = @Content(schema = @Schema(hidden = true)))
+	})
 	@GetMapping
-	public ResponseEntity<Page<RoleResponseDTO>> getAllRoles(@PageableDefault Pageable pageable) {
+	public ResponseEntity<Page<RoleResponseDTO>> getAllRoles(@ParameterObject @PageableDefault Pageable pageable) {
 		return ResponseEntity.ok(roleService.getAllRoles(pageable).map(RoleMapper::fromRoleToResponse));
 	}
 
+	@Operation(summary = "Get Role By Id", description = "Route to get Role By Id", tags = "Role")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Get role by id success!"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Role not found!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class)))
+	})
 	@GetMapping("{id}")
 	public ResponseEntity<RoleResponseDTO> getRoleById(@PathVariable Long id) {
 		Role role = roleService.getRoleById(id);
 		return ResponseEntity.ok(RoleMapper.fromRoleToResponse(role));
 	}
 
+	@Operation(summary = "Delete Role", description = "Route to delete Role", tags = "Role")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Role deleted!", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "400", description = "Role in use!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Role not found!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class)))
+	})
 	@DeleteMapping("{id}")
 	public ResponseEntity<RoleResponseDTO> deleteRole(@PathVariable Long id) {
 		roleService.deleteRole(id);
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "Update Role", description = "Route to update Role", tags = "Role")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Role update!"),
+			@ApiResponse(responseCode = "400", description = "Argument not valid!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
+			@ApiResponse(responseCode = "404", description = "Role not found! or RoleName in use!", content = @Content(schema = @Schema(implementation = ErrorsDTO.class)))
+	})
 	@PutMapping("{id}")
 	public ResponseEntity<RoleResponseDTO> updateRole(@Valid @RequestBody RoleRequestDTO dto,
 			@PathVariable Long id) {
--- src/main/java/eoen/jwtroles/dtos/RoleRequestDTO.java
+++ src/main/java/eoen/jwtroles/dtos/RoleRequestDTO.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +17,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RoleRequestDTO {
     
+    @Schema(example = "RoleName")
     @Column(unique = true)
     @NotBlank(message = "roleName must not be blank!")
     @NotNull(message = "roleName must not be null!")
     private String roleName;
 
+    @Schema(example = "RoleDescription")
     @NotBlank(message = "roleDescription must not be blank!")
     @NotNull(message = "roleDescription must not be null!")
     @Size(min = 3, message = "roleDescription size must be at least 3")
